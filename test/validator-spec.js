f28e7de9c70e5ff340e7e77152d2532103c389dc
Mark Lybrand <mlybrand@gmail.com>
2015-04-11 23:27:57 -0700

    added test as part of 'kata'

--- test/validator-spec.js
+++ test/validator-spec.js
@@ -1,5 +1,8 @@
-var assert = require('assert');
+var assert = require('assert'),
+    validator = require('../lib/validator');
 
 describe('A Validator', function() {
-        
+    it('will return error.nonpositive for not strictly positive numbers', function() {
+        assert.deepEqual(validator(0), ['error.nonpositive']);
+    });
 });
