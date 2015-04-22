4f7e3152172c05f7c33a4ba1967f6fc81355163a
Mark Lybrand <mlybrand@gmail.com>
2015-04-21 20:31:34 -0700

    finished pre-Sinon tests

--- test/validator-spec.js
+++ test/validator-spec.js
@@ -1,31 +1,47 @@
 var chai = require("chai"),
     expect = chai.expect,
-    validator = require('../lib/validator');
-
-function expectedToIncludeErrorWhenInvalid(number, error) {
-    it('like ' + number, function() {
-        expect(validator(number)).to.include(error);
-    });
-}
+    validatorWith = require('../lib/validator'),
+    nonPositiveValidationRule = require('../lib/rules/nonPositive'),
+    nonDivisibleValidationRule = require('../lib/rules/nonDivisible');
 
 describe('A Validator', function() {
+    var validator;
+    before(function() {
+        validator = validatorWith([
+            nonPositiveValidationRule,
+            nonDivisibleValidationRule(3, 'error.three'),
+            nonDivisibleValidationRule(5, 'error.five')
+        ]);
+    });
     it('will return no errors for valid numbers', function() {
         expect(validator(7)).to.be.empty;
     });
 
     describe('will include error.nonpositive for not strictly positive numbers:', function() {
-        expectedToIncludeErrorWhenInvalid(0, 'error.nonpositive');
-        expectedToIncludeErrorWhenInvalid(-2, 'error.nonpositive');
+        it('like 0', function() {
+            expect(validator(0)).to.include('error.nonpositive');
+        });
+        it('like -2', function() {
+            expect(validator(-2)).to.include('error.nonpositive');
+        });
     });
 
     describe('will include error.three for divisible by 3 numbers:', function() {
-        expectedToIncludeErrorWhenInvalid(3, 'error.three');
-        expectedToIncludeErrorWhenInvalid(15, 'error.three');
+        it('like 3', function() {
+            expect(validator(3)).to.include('error.three');
+        });
+        it('like 15', function() {
+            expect(validator(15)).to.include('error.five');
+        });
     });
 
     describe('will include error.five for divisible by 5 numbers:', function() {
-        expectedToIncludeErrorWhenInvalid(5, 'error.five');
-        expectedToIncludeErrorWhenInvalid(15, 'error.five');
+        it('like 5', function() {
+            expect(validator(5)).to.include('error.five');
+        });
+        it('like 15', function() {
+            expect(validator(15)).to.include('error.five');
+        });
     });
 
 });
